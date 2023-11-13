14dfc5411519e1016f4c07d38490f329ac01e802
kwonkk <ig1108@naver.com>
2023-11-13 20:01:04 +0900

    day01

---
+++ _ch01/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author user
+ *
+ */
+module _ch01 {
+}
---
+++ ch02/src/ch02/VarEx2.java
@@ -0,0 +1,26 @@
+package ch02;
+
+public class VarEx2 {
+
+	public static void main(String[] args) {
+		
+		final int score = 100;
+//		score = 200;
+		boolean power = true;
+		
+		byte b = 127;//byte의 범위: -128~127
+		
+		int oct = 010;	//8진수, 10진수로 8
+		int hex = 0x10;	//16진수, 10진수로 16
+		
+		long l = 10_000_000_000L;
+		
+		float f = 3.14f;
+		double d = 3.14;
+		System.out.println(10.);
+		System.out.println(.10);
+		System.out.println(10f);
+		System.out.println(1e3);
+	}
+
+}
