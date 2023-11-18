522787f1b263de00f0496ea710a5498cbd4c8e89
kwonkk <ig1108@naver.com>
2023-11-19 00:38:15 +0900

    day05

--- ch05/src/ch05/Ex5_1.java
+++ ch05/src/ch05/Ex5_1.java
@@ -1,19 +1,35 @@
 package ch05;
 
+import java.util.Arrays;
+
 public class Ex5_1 {
 
 	public static void main(String[] args) {
-//		int score[]; //1. 배열 score를 선언(참조변수)
-//		score = new int[5];	//2. 배열의 생성(int 자정공간 X 5)
 		
-		int score[] = new int[5];	//배열의 선언과 생성을 동시에
-		score[3] = 100;
+		int arr1[] = new int[10];
+		int arr2[] = new int[10];
+		int arr3[] = {100,95,80,70,60};
+		char chArr[] = {'a','b','c','d'};
 		
-		System.out.println("score[0]="+score[0]);
-		System.out.println("score[1]="+score[1]);
-		System.out.println("score[2]="+score[2]);
-		System.out.println("score[3]="+score[3]);
-		System.out.println("score[4]="+score[4]);
+		for(int i = 0; i<arr1.length; i++) {
+			arr1[i] = i+1;	//1~10의 숫자를 순서대로 배열에 넣는다.
+		}
+		for(int i = 0; i<arr2.length; i++) {
+			arr2[i] = (int)(Math.random()*10)+1;	//1~10의 숫자를 순서대로 배열에 넣는다.
+		}
+//		배열에 저장된 값들을 출력
+		for(int i = 0; i<arr1.length; i++) {
+			System.out.print(arr1[i]+",");
+		}
+		System.out.println();
+		
+		System.out.println(Arrays.toString(arr2));
+		System.out.println(Arrays.toString(arr3));
+		System.out.println(Arrays.toString(chArr));
+		System.out.println(arr1);
+		System.out.println(arr2);
+		System.out.println(arr3);
+		System.out.println(chArr);
 		
 	}
 
---
+++ ch05/src/ch05/Ex5_1_tmp.java
@@ -0,0 +1,19 @@
+package ch05;
+
+import java.util.Arrays;
+
+public class Ex5_1_tmp {
+
+	public static void main(String[] args) {
+		int arr[] = {100,95,80,70,60};
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+		
+		System.out.println(Arrays.toString(arr));
+	
+	}
+
+}
