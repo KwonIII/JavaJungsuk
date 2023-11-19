9a0ba8da9426c65b8638e007d450c1ccc44920ff
kwonIII <ig1108@nate.com>
2023-11-19 17:53:09 +0900

    day05

--- ch05/src/ch05/Ex5_1_tmp.java
+++ ch05/src/ch05/Ex5_1_tmp.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class Ex5_1_tmp {
 
 	public static void main(String[] args) {
-		int arr[] = {100,95,80,70,60};
+//		index: 0~3-1, 0~2
+		String strArr[] = {"가위","바위","보"};
+		System.out.println(Arrays.toString(strArr));
 		
-		for(int i=0;i<arr.length;i++)
-		{
-			System.out.println(arr[i]);
+		for(int i = 0;i<10;i++) {
+			int tmp = (int)(Math.random()*3);
+			System.out.println(strArr[tmp]);
 		}
-		
-		System.out.println(Arrays.toString(arr));
 	
 	}
 
