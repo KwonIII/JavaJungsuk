b14d3fe9651d7209221d21d57002b2d6fd5c6de3
kwonkk <ig1108@nate.com>
2023-11-21 16:58:12 +0900

    day07

---
+++ ch06/src/ch06/Ex6_3.java
@@ -0,0 +1,37 @@
+package ch06;
+
+public class Ex6_3 {
+
+	public static void main(String[] args) {
+		System.out.println("Card.width = " + Card.width);
+		System.out.println("Card.height = " + Card.height);
+		
+		Card c1 = new Card();
+		c1.kind = "Heart";
+		c1.number = 7;
+		
+		Card c2 = new Card();
+		c2.kind = "Spade";
+		c2.number = 4;
+		
+		System.out.println("c1은 " + c1.kind + ", " +c1.number + "이며, 크기는 (" + Card.width + ", " + Card.height+")"); 
+		System.out.println("c2는 " + c2.kind + ", " +c2.number + "이며, 크기는 (" + Card.width + ", " + Card.height+")");
+		System.out.println("c1의 width와 height를 각각 50, 80으로 변경합니다.");
+		Card.width = 50;
+		Card.height = 80;
+		
+		System.out.println("c1은 " + c1.kind + ", " +c1.number + "이며, 크기는 (" + Card.width + ", " + Card.height+")"); 
+		System.out.println("c2는 " + c2.kind + ", " +c2.number + "이며, 크기는 (" + Card.width + ", " + Card.height+")");
+		
+		
+	}
+
+}
+class Card{
+	String kind;	//무늬(인스턴스 변수)
+	int number;		//숫자(인스턴스 변수)
+	
+	
+	static int width = 100;	//폭(클래스 변수)
+	static int height = 250;	//높이(클래스 변수)
+}
--- ch06/src/ch06/Hello3.java
+++ ch06/src/ch06/Hello3.java
@@ -1,9 +1,11 @@
 package ch06;
 //하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
-public class Hello3 {}	//소스파일의 이름은 public class 이름과 일치해야 한다.
-class hello3{
+public class Hello3 {
 	public static void main(String[] args) {
 		System.out.println("hello");
 	}
+}	//소스파일의 이름은 public class 이름과 일치해야 한다.
+class hello3{
+	
 }//하나의 소스파일에는 하나의 public class만 허용
 class Hello4{}
